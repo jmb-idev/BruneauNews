7a0baacea7dcce4140a7a18380c98a602afbb86a
idev (Ike Devolder) <i-devolder@jm-bruneau.be>
2011-03-03 15:29:04 +0100

    Database stuff, make caching work + create cache dir when not existing
    Signed-off-by: idev (Ike Devolder) <i-devolder@jm-bruneau.be>

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,17 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
-
-
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
+	protected function _initCacheDir() {
+		$options = $this->getOption('resources');
+		if(isset($options['cachemanager']) &&
+			isset($options['cachemanager']['database']) &&
+			isset($options['cachemanager']['database']['backend']) &&
+			isset($options['cachemanager']['database']['backend']['options']) &&
+			isset($options['cachemanager']['database']['backend']['options']['cache_dir']))
+		{
+			if(!file_exists($options['cachemanager']['database']['backend']['options']['cache_dir']))
+				mkdir($options['cachemanager']['database']['backend']['options']['cache_dir'], 0755, true);
+		}
+	}
 }
 
