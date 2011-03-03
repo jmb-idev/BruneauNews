fc79489a1749ce2aafb141d92153843114d388c2
idev (Ike Devolder) <i-devolder@jm-bruneau.be>
2011-03-03 13:48:23 +0100

    Comment :: added db models table and row
    Signed-off-by: idev (Ike Devolder) <i-devolder@jm-bruneau.be>

---
+++ application/models/db/row/Comment.php
@@ -0,0 +1,4 @@
+<?php
+
+class Comment extends Zend_Db_Table_Row_Abstract {
+}
---
+++ application/models/db/table/Comment.php
@@ -0,0 +1,6 @@
+<?php
+
+class Comment extends Zend_Db_Table_Abstract {
+	protected $_name = 'comment';
+	protected $_rowClass = 'Comment';
+}
