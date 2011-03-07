af5465d9ed822015e60e4645d0529e930b5e4bfb
idev (Ike Devolder) <i-devolder@jm-bruneau.be>
2011-03-07 20:20:13 +0100

    ViewHelpers Date and NewsitemSummary working
    Signed-off-by: idev (Ike Devolder) <i-devolder@jm-bruneau.be>

--- library/Jmb/View/Helper/Date.php
+++ library/Jmb/View/Helper/Date.php
@@ -1,6 +1,19 @@
 <?php
-
+/**
+ * Jmb_View_Helper_Date.
+ * Date view helper, put a string in it, retreive it always in the same format
+ *
+ * @uses Zend_View_Helper_Abstract
+ * @author Ike Devolder <ike DOT devolder AT gmail DOT com>
+ */
 class Jmb_View_Helper_Date extends Zend_View_Helper_Abstract {
+	/**
+	 * give the date
+	 *
+	 * @param string $date
+	 * @access public
+	 * @return formatted date
+	 */
 	public function date($date) {
 		$zndDate = new Zend_Date($date);
 		return $zndDate->get('d MMM y HH:mm:ss');
