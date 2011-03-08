367aa205590d175735773c3ad16987186878ab31
yduq (Yves Duquet) <y-duquet@jm-bruneau.be>
2011-03-08 10:34:46 +0100

    comment table data gateway test
    Signed-off-by: yduq (Yves Duquet) <y-duquet@jm-bruneau.be>

--- application/controllers/CommentController.php
+++ application/controllers/CommentController.php
@@ -6,11 +6,31 @@ class CommentController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+        $bootstrap = Zend_Controller_Front::getInstance()->getParam('bootstrap');
+        $db = $bootstrap->getResource('db');
+
+        $select=$db->select();
+        $select->from('Comment');
+
+        $result = $db->query($select);
+
+		$comments = new Application_Models_Db_Table_Comment($db);
+
+		$rowset = $comments->fetchAll();
+
+
+
+
+        foreach($rowset as $row){
+			Zend_Debug::dump($row, $label=null, $echo=true);
+        }
     }
 
     public function indexAction()
     {
         // action body
+
+
     }
 
 
