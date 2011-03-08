1ddd884a0989ea4d6cb6d61026dde40d81c2f13c
yduq (Yves Duquet) <y-duquet@jm-bruneau.be>
2011-03-08 10:38:05 +0100

    comment table data gateway test
    Signed-off-by: yduq (Yves Duquet) <y-duquet@jm-bruneau.be>

--- application/models/db/table/Comment.php
+++ application/models/db/table/Comment.php
@@ -1,6 +1,6 @@
 <?php
 
-class Application_Models_Db_Table_Comment extends Zend_Db_Table_Abstract {
+class Application_Model_Db_Table_Comment extends Zend_Db_Table_Abstract {
 	protected $_name = 'comment';
 	protected $_rowClass = 'Comment';
 }
