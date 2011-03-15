dc844a5ce9d3425b9637e457fad64c25bce2de63
yduq (Yves Duquet) <y-duquet@jm-bruneau.be>
2011-03-15 14:26:17 +0100

    view bootstrap not necessary
    Signed-off-by: yduq (Yves Duquet) <y-duquet@jm-bruneau.be>

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -13,21 +13,6 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 				mkdir($options['cachemanager']['database']['backend']['options']['cache_dir'], 0755, true);
 		}
 	}
-	
-	protected function _initHtml(){
-		$this->bootstrap('view');
-		$view = $this->getResource('view');
-		$options = $this->getOption('resources');
-		if(isset($options['view']['encoding'])){
-			$view->setEncoding($options['view']['encoding']);
-		}
-		if(isset($options['view']['doctype'])){
-			$view->doctype($options['view']['doctype']);
-		}
-		if(isset($options['view']['contentType'])){
-			//$view->headMeta()->appendHttpEquiv('Content-Type',$options['view']['contentType']);
-		}	
-	}
 
 }
 
