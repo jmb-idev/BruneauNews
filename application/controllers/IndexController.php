722b1f211e5f9789871178ed33ec3149dc3b9fae
idev (Ike Devolder) <i-devolder@jm-bruneau.be>
2011-03-29 19:59:07 +0200

    added some menu thingy
    Signed-off-by: idev (Ike Devolder) <i-devolder@jm-bruneau.be>

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,6 +6,8 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+		$content = "<a href='/admin' title='admin'>admin</a>";
+		$this->view->placeholder('menu')->set($content);
     }
 
     public function indexAction()
