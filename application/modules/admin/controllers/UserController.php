c7270f8223445de99b3431d68ebdd44175bbbb6f
idev (Ike Devolder) <i-devolder@jm-bruneau.be>
2011-03-30 21:34:19 +0200

    UserManagement almost feature complete
    Signed-off-by: idev (Ike Devolder) <i-devolder@jm-bruneau.be>

--- application/modules/admin/controllers/UserController.php
+++ application/modules/admin/controllers/UserController.php
@@ -3,85 +3,73 @@
 class Admin_UserController extends Zend_Controller_Action
 {
 
-    public function init()
-    {
-        /* Initialize action controller here */
+	public function init()
+	{
+		/* Initialize action controller here */
 		$content = "<a href='/admin' title='admin'>admin</a>";
 		$this->view->placeholder('menu')->set($content);
-		$this->flashMessenger = new Zend_Controller_Action_Helper_FlashMessenger();
-		$this->view->messages = $this->flashMessenger->getMessages();    	
-    }
 
-    public function indexAction()
-    {
-        // action body
+		$this->flashMessenger = new Zend_Controller_Action_Helper_FlashMessenger();
+		$this->view->messages = $this->flashMessenger->getMessages();
+	}
+
+	public function indexAction()
+	{
+		// action body
 		$user = new Application_Model_Db_Table_User();
 		$this->view->userlist = $user->getList();
-		Zend_Debug::dump($this->view->userlist);
-    }
+		//Zend_Debug::dump($this->view->userlist);
+	}
 
-    public function createAction()
-    {
+	public function createAction()
+	{
 		$form =$this->view->form = new Application_Form_User();
-		$this->renderScript('/user/edit.phtml');    	
-    }
-
-    public function editAction()
-    {
-        $form =$this->view->form = new Application_Form_User();
-
-        $userTable = new Application_Model_Db_Table_User();
-    	
 		if($this->_request->isPost()){
-        	if($form->isValid($this->_request->getPost())){
-     			$id = $this->_request->getParam('id',0);
-        	    if($id<=0) {
+			if($form->isValid($this->_request->getPost())){
+				$userTable = new Application_Model_Db_Table_User();
+				$userTable->insert($form->getValues());
+			}
+		}
+	}
+
+	public function editAction()
+	{
+		$form =$this->view->form = new Application_Form_User();
+		$userTable = new Application_Model_Db_Table_User();
+
+		if($this->_request->isPost()){
+			if($form->isValid($this->_request->getPost())){
+				$id = $this->_request->getParam('id',0);
+				if($id<=0) {
 					$this->flashMessenger->addMessage('wrong id');
 					$this->_redirect('/admin/user');
 				}
 				else{
-					$userTable->update($form->getValues(), 'id = '.$id);	
-				}    			
-	        }
-        }
-        else{
-        	$id = intval($this->_request->getParam('id', 0));
-        	if($id<=0) {
+					$userTable->update($form->getValues(), 'id = '.$id);
+				}
+			}
+		}
+		else{
+			$id = intval($this->_request->getParam('id', 0));
+			if($id<=0) {
 				$this->flashMessenger->addMessage('wrong id');
 				$this->_redirect('/admin/user');
 			}
 			else{
-		    	if($row = $userTable->getUser($id)){
-    				$form->populate($row->toArray());
-    			}
-			}  	
-        }
-    }    
-    
-    public function updateAction()
-    {
-    	// action body
-    	
-    	
-    	if($this->_request->isPost()) {
-			$data = $this->_request->getPost();
-			if($form->isValid($data)) {
-				$userTable = new Application_Model_Db_Table_User();
-				if($userTable->insert($form->getValues()))
-					$this->flashMessenger->addMessage('your article has ben saved');
-				else
-					$this->flashMessenger->addMessage('there went something wrong with your article');
-
-				$this->_redirect('/admin/news');
+				if($row = $userTable->getUser($id)){
+					$form->populate($row->toArray());
+				}
 			}
-		}    	
-    	
-    }
+		}
+	}
+
+	public function deleteAction()
+	{
+		$id = $this->_request->getParam('id',0);
+		Zend_Debug::dump($id);
+		$this->_forward('index');
+	}
 
 
 }
-
-
-
-
 
