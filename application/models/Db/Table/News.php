fbc4c46bc67ffd59ec8034b9c26d90a22da4b8b6
idev (Ike Devolder) <i-devolder@jm-bruneau.be>
2011-03-30 16:17:09 +0200

    added some more navigation stuff, little more readable layout, ...
    Signed-off-by: idev (Ike Devolder) <i-devolder@jm-bruneau.be>

--- application/models/Db/Table/News.php
+++ application/models/Db/Table/News.php
@@ -7,26 +7,23 @@ class Application_Model_Db_Table_News extends Zend_Db_Table_Abstract{
 	protected $_rowClass = 'Application_Model_Db_Row_News';
 
 	public function getList() {
-		//$select = $this->select(true);
-		//echo $select; die();
 		$select = $this->getDefaultAdapter()->select();
 		$select->from('news')->joinLeft('user', 'news.userId = user.id', array('firstName', 'surName'));
-		//echo $select; die();
-		//Zend_Debug::dump($this->fetchAll('SELECT `news`.* FROM `news`')); die();
 		return $this->getDefaultAdapter()->fetchAll($select);
-		//Zend_Debug::dump($this->fetchAll()); die();
 	}
 
 	public function getEditList() {
 		$select = $this->getDefaultAdapter()->select();
 		$select->from('news', array('id', 'title'));
-		//Zend_Debug::dump($this->getDefaultAdapter()->fetchAll($select)); die();
 		return $this->getDefaultAdapter()->fetchAll($select);
 	}
 
 	public function getDetail($id) {
-		return $this->find($id)->current();
-		//return $this->fetchRow('id = '.$id);
+		$select = $this->getDefaultAdapter()->select();
+		$select->from('news')
+			->where('news.id = '.$id)
+			->joinLeft('user', 'news.userId = user.id', array('firstName', 'surName'));
+		return $this->getDefaultAdapter()->fetchRow($select);
 	}
 
 	public function storeNews() {
